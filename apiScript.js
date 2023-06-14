439e8daea1cd1e4e99404a8e27546456c15dd63c
A. Khachaturyan <114132465+Moberly3@users.noreply.github.com>
2023-06-14 22:52:21 +0200

    2.1 ready version api

--- apiScript.js
+++ apiScript.js
@@ -7,54 +7,38 @@ $(document).ready(function(){
     });
 });
 
-$(document).ready(function(){
-    $("#animation").click(function(){
-      var div = $("#divAnimate");
-      div.animate({height: '300px', opacity: '0.4'}, "slow");
-      div.animate({width: '300px', opacity: '0.8'}, "slow");
-      div.animate({height: '100px', opacity: '0.4'}, "slow");
-      div.animate({width: '100px', opacity: '0.8'}, "slow");
-      div.animate({height: '200px', opacity: '0.2'}, "slow");
-      div.animate({width: '200px', opacity: '0.4'}, "slow");
-      div.animate({height: '100px', opacity: '0.2'}, "slow");
-      div.animate({width: '100px', opacity: '0.4'}, "slow");
-    });
-  });
 
-  const API_URL = `https://api.thecatapi.com/v1/`;
+
+  const API_URL = 'https://api.thecatapi.com/v1/images/search';
   const API_KEY = "live_wnwdnJC3u270mi2WhO3oYqq7jwnwI68g9FfIqoLZDINYq3yRYXZEdL0yA0afCnAv";
   
-  let currentImageToVoteOn;
+  function getRandomCatImage() {
+    fetch(API_URL, {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': API_KEY
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            const catImagesDiv = document.getElementById('catImages');
+            catImagesDiv.innerHTML = '';
 
-  function showImageToVoteOn()
-{
-  
-  const url = `${API_URL}images/search`;
-
-  fetch(url,{headers: {
-    'x-api-key': API_KEY
-  }})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    currentImageToVoteOn = data[0];
-    document.getElementById("image-to-vote-on").src= currentImageToVoteOn.url;
-  });
-
+            data.forEach(imageData => {
+                const image = document.createElement('img');
+                image.src = imageData.url;
+                image.classList.add('catImage');
+                catImagesDiv.appendChild(image);
+            });
+        })
+        .catch(error => {
+            console.log('Error occurred:', error);
+        });
 }
 
-function vote(value)
-{
-  
-  const url = `${API_URL}votes/`;
-  const body = {
-    image_id:currentImageToVoteOn.id,
-    value
-  }
-  fetch(url,{method:"POST",body:JSON.stringify(body),headers: {
-    'content-type':"application/json",
-    'x-api-key': API_KEY
-  }})
-  
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const changeImageButton = document.getElementById('changeImageButton');
+    changeImageButton.addEventListener('click', getRandomCatImage);
+
+    getRandomCatImage();
+});
